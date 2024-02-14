d8afe5f58b63c36dc21e1e69daf067dfd79832a8
ahsanrkabir <ahsanrifat001@gmail.com>
2024-02-15 05:54:27 +0600

    added javascript for game functions

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,12 +1,68 @@
-// function play() {
-//     // hide the homepage section
-//     const homePage = document.getElementById('home-page');
-//     homePage.classList.add('hidden');
-//     // show the playground section
-//     const playGround = document.getElementById('play-ground');
-//     playGround.classList.remove('hidden');
-// }
+// callback function for 'keyup' event
+function handleKeyUpEvent(event) {
+    // key pressed by player
+    const keyPressed = event.key;
 
+    // stop the game if pressed 'Esc'
+    if (keyPressed === 'Escape') {
+        gameOver();
+    }
+
+    // key press expected
+    const currentLetterElement = document.getElementById('current-letter');
+    const currentLetter = currentLetterElement.innerText.toLowerCase();
+
+    // check matched or not
+    if (keyPressed === currentLetter) {
+        // // increase score by 1
+        // const currentScoreElement = document.getElementById('current-score');
+        // const currentScore = parseInt(currentScoreElement.innerText);
+        // const newScore = currentScore + 1;
+        // currentScoreElement.innerText = newScore;
+        const currentScore = getElementValueById('current-score');
+        const newScore = currentScore + 1;
+        setElementValueById('current-score', newScore);
+
+        // start a new round
+        removeBackgroundColorById(currentLetter);
+        continueGame();
+    }
+    else {
+        // // decrease life by 1
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLife = parseInt(currentLifeElement.innerText);
+        // const newLife = currentLife - 1;
+        // currentLifeElement.innerText = newLife;
+        const currentLife = getElementValueById('current-life');
+        const newLife = currentLife - 1;
+        setElementValueById('current-life', newLife);
+
+        //hide playground and show result-card section
+        if (newLife === 0) {
+            gameOver();
+        }
+    }
+}
+
+// start game
+function startGame() {
+    // add event listener for keyup event
+    document.addEventListener('keyup', handleKeyUpEvent);
+
+    // hide homepage and final-score and show only playgrond
+    hideElementById('home-page');
+    hideElementById('score-card');
+    showElementById('play-ground');
+
+    // reset score and life
+    setElementValueById('current-life', 5);
+    setElementValueById('current-score', 0);
+
+    // game function
+    continueGame();
+}
+
+// game function
 function continueGame() {
     // generate a random letter
     const randomLetter = generateRandomLetter();
@@ -19,8 +75,16 @@ function continueGame() {
     setBackgroundColorById(randomLetter);
 }
 
-function play() {
-    hideElementById('home-page');
-    showElementById('play-ground');
-    continueGame();
+// finish game
+function gameOver() {
+    hideElementById('play-ground');
+    showElementById('score-card');
+
+    // update final score
+    const finalScore = getElementValueById('current-score');
+    setElementValueById('final-score', finalScore);
+
+    // remove background color from the last generated letter
+    const currentLetter = getElementTextById('current-letter');
+    removeBackgroundColorById(currentLetter);
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -10,6 +10,38 @@ function showElementById(elementId) {
     element.classList.remove('hidden');
 }
 
+// set background color to the generated letter
+function setBackgroundColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-[#FFA500]');
+}
+
+// remove background color from the generated letter
+function removeBackgroundColorById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-[#FFA500]');
+}
+
+// get element value from text element by id
+function getElementValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValue = parseInt(element.innerText);
+    return elementValue;
+}
+
+// set element value by id
+function setElementValueById(elementId, elementValue) {
+    const element = document.getElementById(elementId);
+    element.innerText = elementValue;
+}
+
+// get element text by id
+function getElementTextById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementText = element.innerText;
+    return elementText;
+}
+
 // generate a random letter from alphabet array using Math
 function generateRandomLetter() {
     // alphabet array
@@ -22,10 +54,4 @@ function generateRandomLetter() {
     // random letter from alphabet array
     const randomLetter = alphabet[randomIndex];
     return randomLetter;
-}
-
-// set background color to the generated letter
-function setBackgroundColorById(elementId) {
-    const element = document.getElementById(elementId);
-    element.classList.add('bg-[#FFA500]');
 }
